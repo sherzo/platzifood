a084ed7759ad3c5e2d5f2d3d53fd84ad611eeafe
Saul Florez <sflorez@distritohosting.com>
2020-06-09 22:59:32 -0400

    📦 NEW: Creando header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      }
+    },
   },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "disabled"],
